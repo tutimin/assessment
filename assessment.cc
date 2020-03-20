ead43e042a088f157ab86d5ef3bfbe5540c034bc
tutimin <62420555+tutimin@users.noreply.github.com>
2020-03-20 17:21:04 +0900

    Create assessment.cc

---
+++ assessment.cc
@@ -0,0 +1,17 @@
+body {
+    background-color: #04A6EB;
+    color: #fdffff;
+    width: 500px;
+    margin-right: auto;
+    margin-left: auto;
+}
+button{
+    padding: 5px 20px;
+    background-color: #337AB7;
+    color: #fdffff;
+    border-style: none;
+
+}
+input{
+    height: 24px;
+}
